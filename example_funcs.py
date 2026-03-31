e9ecbb2c63d8e610cfc1f8f45b7834bc6c51216b
Dmitriy Sulzhuts <dsulzhits@gmail.com>
2026-03-31 19:37:40 +0300

    Fix func: - divide(a, b)

--- example_funcs.py
+++ example_funcs.py
@@ -11,6 +11,8 @@ def subtract(a, b):
 
 
 def divide(a, b):
+    if b == 0:
+        raise ValueError('На ноль делить нельзя!')
     return a / b
 
 
@@ -19,3 +21,7 @@ if __name__ == '__main__':
     print(multiply(5, 3))
     print(subtract(5, 3))
     print(divide(10, 2))
+    try:
+        divide(10, 0)
+    except ValueError as e:
+        print(e)
